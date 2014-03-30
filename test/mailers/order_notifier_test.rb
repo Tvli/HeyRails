a37b0b98b59d6cf33f8db45cbaa48d589d2ced4b
Tvli <526864480@qq.com>
2014-03-30 16:46:57 +0100

    Mailer test

--- test/mailers/order_notifier_test.rb
+++ test/mailers/order_notifier_test.rb
@@ -3,18 +3,18 @@ require 'test_helper'
 class OrderNotifierTest < ActionMailer::TestCase
   test "received" do
     mail = OrderNotifier.received(orders(:one))
-    assert_equal "Received", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_equal "Teng's Store Order Confirmation", mail.subject
+    assert_equal ["526864480@qq.com"], mail.to
+    assert_equal ["tenglichina@gmail.com"], mail.from
+    assert_match "1 x Breed Love Guitar", mail.body.encoded
   end
 
   test "shipped" do
-    mail = OrderNotifier.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderNotifier.shipped(orders(:one))
+    assert_equal "Teng's Store Order Shipped", mail.subject
+    assert_equal ["526864480@qq.com"], mail.to
+    assert_equal ["tenglichina@gmail.com"], mail.from
+    assert_match /<td>1&times;<\/td>\s*<td>Breed Love Guitar<\/td>/, mail.body.encoded
   end
 
 end
